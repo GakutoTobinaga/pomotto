91937c065a28e250cc4b6ff66dbd6a4c423c4685
gtobinaga-sparkleai <gtobinaga@sparkleai.co.jp>
2024-03-21 23:49:10 +0900

    delete testnet

--- app/testnet/page.tsx
+++
@@ -1,9 +0,0 @@
-import Tomato from '@/components/logos/Tomato';
-import TomatoTimer from '@/components/timers/TomatoTimer';
-export default async function Testnet() {
-  return (
-    <>
-      <div>OK</div>
-    </>
-  );
-}
